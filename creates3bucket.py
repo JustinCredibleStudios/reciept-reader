2ae567747b9978b4d0392daec4bcb567d9b4caac
JustinCredible Studios <justincredible219@gmail.com>
2024-10-06 13:29:59 -0400

    boto3 working

--- creates3bucket.py
+++ creates3bucket.py
@@ -3,4 +3,34 @@
 # Date: 9/30/2024
 
 # Importing Libraries
-print("Creating s3 Bucket")
+import logging
+import boto3
+from botocore.exceptions import ClientError
+
+#Defining Variables
+bucket_name = 'recieptreader'
+region = 'us-east-1'
+
+# Defining functions 
+def create_bucket(bucket_name, region):
+    """
+    Create an S3 bucket in the specified region.
+
+    :param bucket_name: Bucket to create
+    :param region: String region to create bucket in, e.g., 'us-west-2'
+    :return: True if bucket created successfully, else False
+    """
+    # Create bucket
+    try:
+        if region is None:
+            s3_client = boto3.client('s3')
+            s3_client.create_bucket(Bucket=bucket_name)
+        else:
+            s3_client = boto3.client('s3', region_name=region)
+            location = {'LocationConstraint': region}
+            s3_client.create_bucket(Bucket=bucket_name,
+                                    CreateBucketConfiguration=location)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
