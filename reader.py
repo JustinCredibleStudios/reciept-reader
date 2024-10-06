26c5ce103e48555b71d076155bb14da4e4829f57
JustinCredible Studios <justincredible219@gmail.com>
2024-10-06 18:33:44 -0400

    Merged analysis to main script

--- reader.py
+++ reader.py
@@ -5,13 +5,11 @@
 import os
 from tkinter import *
 from tkinter import filedialog
-import subprocess
-
-# import creates3bucket
+import boto3
 
 # Defining Global Variables
 destination_folder = "C:\\Users\\Jusrin S. Millero\\OneDrive\\Desktop\\CodeProjects\\Receipt_Reader\\files"
-
+ 
 
 
 # Defining Functions
@@ -25,6 +23,7 @@ def upload_file():
             with open(os.path.join(destination_folder, os.path.basename(file_path)), "wb") as destination_file:
                 with open(file_path, "rb") as source_file:
                     destination_file.write(source_file.read())
+                    return destination_file
         else:
             print("Invalid file type. Please upload a PDF file.")
         
@@ -41,8 +40,26 @@ def check_folder():
     if os.path.exists(destination_folder):
         if not os.listdir(destination_folder):
             print("Folder is empty")
-        else:
-            subprocess.run(["python", "textanalysis.py"])
+            
+def textanalysis():
+    file_name = "files\\supply_store_receipt.pdf"
+    
+    document = file_name
+    
+    textract = boto3.client('textract')
+    
+    response = textract.analyze_document(
+        Document={
+            'Bytes': open(document, 'rb').read(),
+        },
+        FeatureTypes=['TABLES', 'FORMS'],
+    )
+    
+    with open("output.txt", "w") as f:
+
+        for item in response["Blocks"]:
+            if item["BlockType"] == "LINE":
+                print (item["Text"], file=f)
    
 
 # Creating the window
@@ -53,6 +70,9 @@ window.title("Receipt Reader")
 window.geometry("500x500")
 
 button = Button(window, text="Upload Receipt", command=upload_file)
+button.pack()
+
+button = Button(window, text="anaylze", command=textanalysis)
 button.pack()
 
 # Drawing the window
